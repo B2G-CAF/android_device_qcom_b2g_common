d0f47f7e6b0e720b9315a47a728bc04dda4f87db
Alhad Purnapatre <alhadp@codeaurora.org>
2012-07-27 18:59:31 -0700

    xpidl.mk modifications
    1. Renamed to xpcom.mk
    2. Removed install.rdf and chrome.manifest creation (do it manually)
    3. Add rules to auto install Gecko libs into Android obj dirs
    4. Proper namespacing and variable cleanup
    5. Generate empty notice file rules for Gecko static libraries
    
    Change-Id: I896f787e849d1c08be6886c8ef27daabccd72ff5

--- patch/all/build/xpidl-makefile.sh
+++
@@ -1,3 +0,0 @@
-MYLOC=$(dirname ${BASH_SOURCE[0]})
-cp ${MYLOC}/xpidl.mk ${MYLOC}/create_install_rdf ${MYLOC}/create_chrome_manifest core/
-git_add core/xpidl.mk core/create_install_rdf core/create_chrome_manifest

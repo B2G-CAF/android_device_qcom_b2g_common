039305cbdde1ce10716db5798a925825120eff38
Diego Wilson <dwilson@codeaurora.org>
2012-08-10 15:55:19 -0700

    Add touchscreen configuration file for ft5x06_ts
    Change-Id: I20177577030522cf5d4a3d4ce21fdb8b609dbd79

--- keymaps/keymaps.mk
+++ keymaps/keymaps.mk
@@ -28,8 +28,9 @@
 # QRD7x27a_SKU1 touchscreen config
 PRODUCT_COPY_FILES += device/qcom/b2g_common/keymaps/generic.idc:system/usr/idc/sensor00fn11.idc
 
-# QRD7x27a_SKU3 touchscreen config
+# QRD7x27a_SKU3 touchscreen configs
 PRODUCT_COPY_FILES += device/qcom/b2g_common/keymaps/generic.idc:system/usr/idc/ft5x0x_ts.idc
+PRODUCT_COPY_FILES += device/qcom/b2g_common/keymaps/generic.idc:system/usr/idc/ft5x06_ts.idc
 
 # 8960 LIQUID
 PRODUCT_COPY_FILES += device/qcom/b2g_common/keymaps/generic.idc:system/usr/idc/atmel_mxt_ts.idc

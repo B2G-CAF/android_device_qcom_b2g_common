e409ab55e65defac8a1560f848fa658361110b09
Michael Vines <mvines@codeaurora.org>
2012-09-06 19:16:40 -0700

    Update copyright
    Change-Id: Ifa1c60b1a98d4d3c78c5f9cf7912d1e5ae00c686

--- treeid.sh
+++ treeid.sh
@@ -3,7 +3,7 @@
 # Attempt to identify the Android tree in use.  On success one or more
 # tree identifiers are output to stdout.
 #
-# Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+# Copyright (c) 2012, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are

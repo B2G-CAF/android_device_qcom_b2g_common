22a37b525022bd4e0b56952cb56d735caa678983
Diego Wilson <dwilson@codeaurora.org>
2013-09-20 12:40:05 -0700

    Remove remove-media-files.sh
    Landed on platform/frameworks/base[b2g_ics_1.2]
    
    Change-Id: Ice18dd40abb382bd23a29bcc7e31827677bb2244

--- patch/ics/frameworks/base/remove-media-files.sh
+++
@@ -1,6 +0,0 @@
-# Remove unused media files from system image
-echo > data/videos/VideoPackage1.mk
-git_add data/videos/VideoPackage1.mk
-echo > data/sounds/OriginalAudio.mk
-git_add data/sounds/OriginalAudio.mk
-git_rm data/sounds/AllAudio.mk

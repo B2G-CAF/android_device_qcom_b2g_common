419c0d138caa2a629441f04fd1da78f601a6c70a
Diego Wilson <dwilson@codeaurora.org>
2013-09-20 12:40:05 -0700

    Remove remove-dalvik.sh
    Landed on platform/frameworks/base[b2g_ics_1.2]
    
    Change-Id: I4355b3ca0caec46298649f3a2ae7e899615ee069

--- patch/ics/frameworks/base/remove-dalvik.sh
+++
@@ -1,5 +0,0 @@
-git_rm $(find libs/rs -name Android.mk)
-git_rm $(find graphics/jni -name Android.mk)
-git_rm $(find graphics/tests -name Android.mk)
-git_rm $(find core/tests -name Android.mk)
-git_rm $(find packages -name Android.mk)

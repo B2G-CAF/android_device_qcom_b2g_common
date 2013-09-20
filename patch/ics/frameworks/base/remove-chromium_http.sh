7ab7959d8b086067d5951823a6ae4c78bcd85a31
Diego Wilson <dwilson@codeaurora.org>
2013-09-20 12:40:05 -0700

    Remove remove-chromium_http.sh
    Landed on platform/frameworks/base[b2g_ics_1.2]
    
    Change-Id: I375b89ea820e5c26e09e66a0e3832b780271b062

--- patch/ics/frameworks/base/remove-chromium_http.sh
+++
@@ -1,5 +0,0 @@
-if [[ -d media/libstagefright/chromium_http ]]; then
-   # chromium_http fails to compile when the chrome HTTP stack is
-   # disabled...
-   git_rm media/libstagefright/chromium_http/Android.mk
-fi
